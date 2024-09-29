18a82f5583cfaa67611ed5268223e6fe5f6dbc22
Mariia <90716920+Mariia-Petlovana-91@users.noreply.github.com>
2024-09-30 00:44:03 +0200

    add oll functional

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,7 +4,6 @@ import Description from '../Description/Description';
 import Feedback from '../Feedback/Feedback';
 import Options from '../Options/Options';
 import Notification from '../Notification/Notification';
-import css from '../App/App.module.css';
 import { FiDatabase } from 'react-icons/fi';
 
 export default class App extends React.Component {
@@ -14,31 +13,62 @@ export default class App extends React.Component {
     bad: 0,
   };
 
-  updateFeedback = (feedbackType) => {
-    this.setState((prevState) => ({
+componentDidMount() {
+  const savedFeedback = JSON.parse(localStorage.getItem('feedback')) ?? {};
+ 
+  this.setState({
+    good: savedFeedback.good ?? 0,
+    neutral: savedFeedback.neutral ?? 0,
+    bad: savedFeedback.bad ?? 0,
+  });
+}
+
+updateFeedback = (feedbackType) => {
+  this.setState((prevState) => {
+    const updatedState = {
       [feedbackType]: prevState[feedbackType] + 1,
-    }));
+    };
+  
+    localStorage.setItem('feedback', JSON.stringify({ ...prevState, ...updatedState }));
+      
+      return updatedState;
+  });
+};
+
+  handleReset = () => {
+    this.setState({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    },
+    () => {
+      localStorage.removeItem('feedback');
+    })
   };
 
   render() {
+    const { good, neutral, bad } = this.state;
+    const totalFeedback = good + neutral + bad;
     return (
-      <Section>
-	  <Description />
+    <Section>
+	    <Description />
 		    
-        <Feedback
-          handleGood={() => this.updateFeedback('good')}
-          handleNeutral={() => this.updateFeedback('neutral')}
-          handleBad={() => this.updateFeedback('bad')}
-	  />
-		    
+      <Feedback
+        handleGood={() => this.updateFeedback('good')}
+        handleNeutral={() => this.updateFeedback('neutral')}
+        handleBad={() => this.updateFeedback('bad')}
+	    />
+		
+      {totalFeedback !== 0 ?
         <Options
           good={this.state.good}
           neutral={this.state.neutral}
           bad={this.state.bad}
-	  />
-
-	 <Notification />
-      </Section>
+          totalFeedback={totalFeedback}
+          handleReset={this.handleReset}
+        /> : <Notification />
+      }
+    </Section>
     );
   }
 }
--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,14 +1,15 @@
 import css from '../Options/Options.module.css';
 
-export default function Options({ good, neutral, bad, handlereset }) {
+export default function Options({ good, neutral, bad, handleReset, totalFeedback }) {
 	return (
 		<div className={css.options__container}>
 		      <ul className={css.options__list}>
 		        <li className={css.options__item}>Good: {good}</li>
 		        <li className={css.options__item}>Neutral: {neutral}</li>
-		        <li className={css.options__item}>Bad: {bad}</li>
+			  <li className={css.options__item}>Bad: {bad}</li>
+			  <li className={(Math.round((good / totalFeedback) * 100)) >= 50? css.options__psitive : css.options__negative}>Positive: { Math.round((good / totalFeedback) * 100)}%</li>
 			</ul>
-			<button className={css.options__btn} onClick={handlereset} type='button'>Reset</button>
+			<button className={css.options__btn} onClick={handleReset} type='button'>Reset</button>
 		</div>
 	)
 }
